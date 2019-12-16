c8e1bdad68dc6a63851916a31d770bfac978eaf6
lon91ong <lon91ong@gmail.com>
2019-12-16 19:26:40 +0800

    falcon的表单数据解析不如flask完善! 若快的图片采用表单上传的方式, 还没有支持 不影响bypass中联众打码的支持

--- web.py
+++ web.py
@@ -16,23 +16,29 @@ class Resource(object):
     def on_post(self, req, resp):
         resp.content_type = 'application/json'
         print('Path:', req.path)
-        if req.path == '/check-points':
-            resp.media = {"code":0,"message":"","data":{"userPoints":10000, "availablePoints":8000, "lockPoints":2000}}
-        elif req.path == '/upload':
+        if req.path == '/check-points' or req.path == '/info.json':
+            resp.media = {"code":0,"Score":10000,"data":{"userPoints":10000, "availablePoints":8000, "lockPoints":2000}}
+        elif req.path == '/upload' or req.path == '/create.json':
             try:
-                img = b64decode(load(req.stream)['captchaData'])
-            except Exception:
+                img = b64decode(load(req.stream)['captchaData']) if req.path == '/upload' \
+                else req.media.get('username')
+                #print(img)
+            except Exception as e:
+                print("error:",e.args[0])
                 pass
             result = Predict.share().get_coordinate(img,True)
-            resp.body = dumps({"ts":2,"code":0,"message":"","data":{"recognition":result}})
-        elif req.path == '/report-error':
-            resp.media = {"code":0,"message":"","data":{"result":True}}
+            resp.body = dumps({"ts":2,"code":0,"Result":result.replace('|',','),"data":{"recognition":result}})
+        elif req.path == '/report-error' or req.path == '/reporterror.json':
+            resp.media = {"code":0,"Error":1,"data":{"result":True}}
         resp.status = falcon.HTTP_200
 
 class Web:
     def run(self):
         svrRes = Resource()
         app = falcon.API()
+        app.add_route('/info.json', svrRes)
+        app.add_route('/create.json', svrRes)
+        app.add_route('/reporterror.json', svrRes)
         app.add_route('/check-points', svrRes)
         app.add_route('/upload', svrRes)
         app.add_route('/report-error', svrRes)
