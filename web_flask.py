b18d37385b3c19ff4b881c575e671335a976faeb
lon91ong <lon91ong@gmail.com>
2019-12-16 19:23:33 +0800

    同时支持bypass使用的联众打码 和 订票助手使用的若快打码

--- web_flask.py
+++ web_flask.py
@@ -12,6 +12,7 @@ class Web:
     def run(self):
         app.run(**Config.WEB)
 
+# 联众打码
 @app.route('/check-points', methods=['POST'], strict_slashes=False)
 def check():
     return jsonify({"code":0,"message":"","data":{"userPoints":10000, "availablePoints":8000, "lockPoints":2000}})
@@ -29,6 +30,25 @@ def upload():
         pass
     result = Predict.share().get_coordinate(img,True)
     return jsonify({"ts":2,"code":0,"message":"","data":{"recognition":result}})
+
+# 若快打码
+@app.route('/info.json', methods=['POST'], strict_slashes=False)
+def info():
+    return jsonify({"code":0,"Score":10000})
+
+@app.route('/reporterror.json', methods=['POST'], strict_slashes=False)
+def reperr():
+    return jsonify({"code":0,"Error":1})
+
+@app.route('/create.json', methods=['POST'], strict_slashes=False)
+def create():
+    try:
+        img = request.files['image'].read()
+    except Exception as e:
+        print("error:",e.args[0])
+        pass
+    result = Predict.share().get_coordinate(img)
+    return jsonify({"code":0,"Result":str(result)[1:-1]})
 
 if __name__ == '__main__':
     Web().run()
