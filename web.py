107fef45da8d6b0de3afe36e04fe59be1ff670e7
lon91ong <lon91ong@gmail.com>
2019-12-17 09:54:08 +0800

    重新扶正falcon的位子 用cgi.FieldStorage解析form_data数据

--- web.py
+++ web.py
@@ -1,54 +1,46 @@
-from base64 import b64decode
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-12-13 14:24:03
+# @Author  : lon91ong (lon91ong@gmail.com)
+# @Link    : 
+# @Version : $Id$
 
-from flask import Flask, jsonify, request
+import falcon
 from config import Config
-from json import loads
+from json import dumps,load
+from waitress import serve
+from base64 import b64decode
+from cgi import FieldStorage
 from ocr.ml_predict import Predict
 
-app = Flask(__name__)
-
+class Resource(object):
+    def on_post(self, req, resp):
+        resp.content_type = 'application/json'
+        print('Path:', req.path)
+        if req.path in ['/check-points', '/info.json']:
+            resp.media = {"code":0,"Score":10000,"data":{"userPoints":10000, "availablePoints":8000, "lockPoints":2000}}
+        elif req.path in ['/upload', '/create.json']:
+            try:
+                img = b64decode(load(req.stream)['captchaData']) if req.path == '/upload' \
+                else FieldStorage(fp=req.stream, environ=req.env).getvalue('image')
+            except Exception as e:
+                print("error:",e.args[0])
+                pass
+            result = Predict.share().get_coordinate(img,req.path == '/upload')
+            resp.body = dumps({"ts":2,"code":0,"Result":str(result)[1:-1],"data":{"recognition":result}})
+        elif req.path in ['/report-error', '/reporterror.json']:
+            resp.media = {"code":0,"Error":1,"data":{"result":True}}
+        resp.status = falcon.HTTP_200
 
 class Web:
     def run(self):
-        app.run(**Config.WEB)
-
-# 联众打码
-@app.route('/check-points', methods=['POST'], strict_slashes=False)
-def check():
-    return jsonify({"code":0,"message":"","data":{"userPoints":10000, "availablePoints":8000, "lockPoints":2000}})
-
-@app.route('/report-error', methods=['POST'], strict_slashes=False)
-def repErr():
-    return jsonify({"code":0,"message":"","data":{"result":True}})
-
-@app.route('/upload', methods=['POST'], strict_slashes=False)
-def upload():
-    try:
-        img = b64decode(loads(str(request.stream.read(),encoding='utf-8'))['captchaData'])
-    except Exception as e:
-        print("error:",e.args[0])
-        pass
-    result = Predict.share().get_coordinate(img,True)
-    return jsonify({"ts":2,"code":0,"message":"","data":{"recognition":result}})
-
-# 若快打码
-@app.route('/info.json', methods=['POST'], strict_slashes=False)
-def info():
-    return jsonify({"code":0,"Score":10000})
-
-@app.route('/reporterror.json', methods=['POST'], strict_slashes=False)
-def reperr():
-    return jsonify({"code":0,"Error":1})
-
-@app.route('/create.json', methods=['POST'], strict_slashes=False)
-def create():
-    try:
-        img = request.files['image'].read()
-    except Exception as e:
-        print("error:",e.args[0])
-        pass
-    result = Predict.share().get_coordinate(img)
-    return jsonify({"code":0,"Result":str(result)[1:-1]})
+        svrRes = Resource()
+        app = falcon.API()
+        apilst = ['/info.json','/create.json','/reporterror.json','/check-points', '/upload', '/report-error']
+        for apistr in apilst:
+            app.add_route(apistr, svrRes)
+        
+        serve(app, listen = Config.WEB['host']+':'+str(Config.WEB['port']))
 
 if __name__ == '__main__':
     Web().run()
